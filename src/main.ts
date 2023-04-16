bc8a9b5ae286bf6a700d4600ae93816af0efe415
Mohamed El omary <elomarymohamed72@gmail.com>
2023-04-16 21:08:50 +0000

    feat: fixing file too large issue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import bodyParser = require('body-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.enableCors();
+
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
